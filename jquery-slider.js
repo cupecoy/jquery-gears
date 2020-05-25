e56c7f1256adf4bbe48f1e47ce394f53b00ba547
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2020-05-25 17:46:57 +0300

    implemented slider widget

---
+++ jquery-slider.js
@@ -0,0 +1,77 @@
+/**
+ * @author Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
+ * @since May 25, 2020
+ */
+(function($) {
+	'use strict';
+
+	$.widget('gears.slider', {
+		options: {
+			header: '> :nth-child(odd)'
+		},
+
+		top_: null,
+		bottom_: null,
+		sections_: null,
+		sectionsHeight_: 0,
+
+		_create: function() {
+			const self = this;
+
+			this._super();
+
+			this.sections_ = $(this.options.header, this.element).addClass('g-slider-header')
+				.each(function(index) {
+					$(this)
+						.attr('data-slider-index', index)
+						.data('g-slider-header', {
+							header: $(this).clone(),
+							offset: self.sectionsHeight_
+						});
+
+					self.sectionsHeight_ += $(this).outerHeight();
+				});
+
+			this.top_ = $('<div class="g-slider-top"></div>').appendTo(this.element);
+			this.bottom_ = $('<div class="g-slider-bottom"></div>').appendTo(this.element);
+
+			this.element
+				.addClass('g-slider')
+				.on('click', '.g-slider-header', function() {
+					const i = $(this).attr('data-slider-index');
+					self.show(self.sections_.eq(+i));
+				})
+				.on('scroll resize update', function() {
+					self.update();
+				});
+		},
+
+		show: function(section) {
+			const d = $(section).data('g-slider-header');
+			const p = $(section).position();
+
+			this.element.scrollTop(p.top - d.offset + this.element.scrollTop());
+		},
+
+		update: function() {
+			const self = this;
+
+			const scroll = this.element.scrollTop();
+			const height = this.element.innerHeight();
+
+			this.top_.empty().css('top', scroll + 'px');
+			this.bottom_.empty().css('bottom', -scroll + 'px');
+			
+			this.sections_.each(function() {
+				const d = $(this).data('g-slider-header');
+				const p = $(this).position();
+
+				if (p.top < d.offset)
+					self.top_.append(d.header);
+
+				if (p.top > height - self.sectionsHeight_ + d.offset)
+					self.bottom_.append(d.header);
+			});
+		}
+	});
+})(jQuery);
