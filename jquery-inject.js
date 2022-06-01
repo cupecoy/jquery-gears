054114cbf181142d189e98c26c3ef838a1e0374a
Yuri Plaksyuk <yuri.plaksyuk@chestnutcorp.com>
2022-06-01 13:02:07 +0300

    Updated injection logic

--- jquery-inject.js
+++ jquery-inject.js
@@ -15,69 +15,73 @@
 	};
 
 
-	jQuery.fn.inject = function(o, x) {
+	jQuery.fn.inject = function(o, t) {
 		return this.each(function() {
-			for (let i = this.attributes.length -1; i >= 0; --i) {
-				const a = this.attributes[i];
+			if (this.nodeType == 1) {
+				for (let i = this.attributes.length -1; i >= 0; --i) {
+					const a = this.attributes[i];
 
-				if (!/^data-inject/.test(a.name)) {
-					let v = a.value, s = '', r = null;
-					while ((r = v.match(/^([^$]*)\${([\w\.]+)}(.*)$/)) !== null) {
-						s += r[1] + path(o, r[2]); v = r[3];
+					if (!/^data-inject/.test(a.name)) {
+						let v = a.value, s = '', r = null;
+						while ((r = v.match(/^([^$]*)\${([\w\.]+)}(.*)$/)) !== null) {
+							s += r[1] + path(o, r[2]); v = r[3];
+						}
+						this.setAttribute(a.name, s + v);
 					}
-					this.setAttribute(a.name, s + v);
 				}
-			}
 
-			const r = attr(this, 'data-inject-ref');
-			if (r) $('#' + r, x).clone().appendTo(this);
+				const r = attr(this, 'data-inject-ref');
+				if (r) $('#' + r).clone().appendTo(this);
 
-			const i = attr(this, 'data-inject-if');
-			if (i) {
-				const v = path(o, i);
-				if (v === undefined) {
-					$(this).remove();
-					return;
-				}
-			}
-
-			const j = attr(this, 'data-inject');
-			if (j) {
-				const v = path(o, j);
-				if (Array.isArray(v)) {
-					const c = $('<div></div>');
-
-					let m, f, l;
-					if ($(this).children('[data-inject-item]').appendTo(c).length > 0) {
-						m = c.children('[data-inject-item=middle]');
-						f = c.children('[data-inject-item=first]');
-						l = c.children('[data-inject-item=last]');
-
-						if (f.length == 0) f = m;
-						if (l.length == 0) l = m;
-
+				const i = attr(this, 'data-inject-if');
+				if (i) {
+					const v = path(o, i);
+					if (v === undefined) {
+						$(this).remove();
+						return;
 					}
+				}
+
+				const j = attr(this, 'data-inject');
+				if (j) {
+					const v = path(o, j);
+					if (Array.isArray(v)) {
+						const c = $('<div></div>');
+
+						let m, f, l;
+						if ($(this).children('[data-inject-item]').appendTo(c).length > 0) {
+							m = c.children('[data-inject-item=middle]');
+							f = c.children('[data-inject-item=first]');
+							l = c.children('[data-inject-item=last]');
+
+							if (f.length == 0) f = m;
+							if (l.length == 0) l = m;
+
+						}
+						else
+							m = f = l = $(this).children().appendTo(c);
+
+						for (let i = 0; i < v.length; ++i) {
+							f.clone().inject(v[i], c).appendTo(this);
+							f = i + 2 < v.length ? m : l;
+						}
+					}
+					else if ($.isPlainObject(v))
+						$(this).children().inject(v, t);
+					else if (v === undefined)
+						$(this).remove();
+					else if (this.tagName === 'IMG')
+						this.src = v;
+					else if (this.tagName === 'A')
+						this.href = v;
+					else if (this.tagName === 'INPUT' || this.tagName === 'SELECT')
+						$(this).val(v, t);
 					else
-						m = f = l = $(this).children().appendTo(c);
-
-					for (let i = 0; i < v.length; ++i) {
-						f.clone().inject(v[i], c).appendTo(this);
-						f = i + 2 < v.length ? m : l;
-					}
+						$(this).text(v);
 				}
-				else if ($.isPlainObject(v))
-					$(this).children().inject(v, x);
-				else if (v === undefined)
-					$(this).remove();
-				else if (this.tagName === 'IMG')
-					this.src = v;
-				else if (this.tagName === 'A')
-					this.href = v;
 				else
-					$(this).append(v);
+					$(this).children().inject(o, t);
 			}
-			else
-				$(this).children().inject(o, x);
 		});
 	};
 })(jQuery);
